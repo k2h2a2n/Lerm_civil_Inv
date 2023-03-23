b43fd7aa0ffed14cf7d0176bc6931dc53efbaac6
k2h2a2n <afzal.khan@esehat.org>
2023-03-23 11:25:25 +0000

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{'name':'Kes_Invoicing',
+ 'summary': "Invoicing",
+ 'author': "Usman Shaikhnag and Khan Afzal", 
+ 'website': "http://www.esehat.org", 
+ 'category': 'Uncategorized', 
+ 'version': '13.0.1', 
+ 'depends':['base' , 'contacts'],
+ 'data': [
+    
+    ]
+}
