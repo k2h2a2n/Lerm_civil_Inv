38547108c986815f8673e074fc5f3b7250257e09
UsmanShaikhnagofficial <usman.shaikhnag@esehat.org>
2023-03-24 17:26:35 +0000

    default contact

--- models/customer.py
+++ models/customer.py
@@ -5,8 +5,17 @@ class Customer(models.Model):
 
     pan_no = fields.Char(string="PAN No")
 
+
 class AccountMoveInherited(models.Model):
     _inherit = 'account.move'
 
     project_name = fields.Char(string="Project Name")
+    po_number = fields.Char(string="PO Number")
+    customer_reference = fields.Char(string="Customer Reference")
     default_contact = fields.Many2one('res.partner',string="Default Contact")
+
+
+class AccountMoveLineInherited(models.Model):
+    _inherit = 'account.move.line'
+
+    report_no = fields.Char(string="Report Number")
